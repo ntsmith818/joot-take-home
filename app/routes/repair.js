005604c1f0d546c8c1a06aafd1233ca0b450c21b
Nicholas Smith <nicholassmith@Nicholass-MacBook-Pro.local>
2021-04-25 20:58:25 -0700

    edit to incorrect route param

--- app/routes/repair.js
+++ app/routes/repair.js
@@ -7,6 +7,6 @@ router.get('/', repairControllers.getRepairs)
 router.get('/:repairId', repairControllers.getRepairs)
 router.post('/:customerId/create', repairControllers.createRepair)
 router.delete('/:repairId/customer/:customerId/delete', repairControllers.deleteRepair)
-router.put('/:inventoryId/update',  repairControllers.updateRepair)
+router.put('/:repairId/update',  repairControllers.updateRepair)
 
 module.exports = router
